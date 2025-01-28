561815a87703953ff024a5e80ee5c83c2314b798
henryle <le000379@umn.edu>
2025-01-28 00:08:30 -0600

    j

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,5 +7,5 @@ import os
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'))
 
 # Get the DATABASE_URL from the environment
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")''
 
