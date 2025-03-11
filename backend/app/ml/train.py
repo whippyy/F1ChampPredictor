d8dab438cc08cf18de0c1e958c0a58fa828e84c1
henryle <le000379@umn.edu>
2025-03-10 21:12:33 -0500

    trying to fix train

--- backend/app/ml/train.py
+++ backend/app/ml/train.py
@@ -1,9 +1,8 @@
 import pandas as pd
 import numpy as np
-from tensorflow.keras import Sequential
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
-
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
